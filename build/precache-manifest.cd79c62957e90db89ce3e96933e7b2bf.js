fffdc3a05d50176b7401093778a712a75e4dfefa
wilckg <37071851+wilckg@users.noreply.github.com>
2020-08-18 21:55:13 -0300

    Build App

---
+++ build/precache-manifest.cd79c62957e90db89ce3e96933e7b2bf.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aac776c7e0951ab9a528622495d85b94",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3801971b34f855178fa6",
+    "url": "/static/css/2.1a8daee9.chunk.css"
+  },
+  {
+    "revision": "bba1e17b092a7d40efd5",
+    "url": "/static/css/main.6ef66a88.chunk.css"
+  },
+  {
+    "revision": "3801971b34f855178fa6",
+    "url": "/static/js/2.d3508212.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d3508212.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bba1e17b092a7d40efd5",
+    "url": "/static/js/main.761d5ca8.chunk.js"
+  },
+  {
+    "revision": "16d50208a04408d1bf24",
+    "url": "/static/js/runtime-main.23041e79.js"
+  },
+  {
+    "revision": "7b4706697570f921fe3b70b91e64f9fb",
+    "url": "/static/media/instagram-icon.7b470669.png"
+  },
+  {
+    "revision": "e300ea684f0e3e91629af21059a7c460",
+    "url": "/static/media/laptop_templates.e300ea68.png"
+  },
+  {
+    "revision": "e35879275bddcda3a419a9599e5e9e1c",
+    "url": "/static/media/logo_footer.e3587927.png"
+  },
+  {
+    "revision": "77870428dbce601e257b96d801c267ef",
+    "url": "/static/media/logo_topo.77870428.png"
+  },
+  {
+    "revision": "01f64e3038f186936d545e80269101f1",
+    "url": "/static/media/lucas-silva.01f64e30.jpg"
+  },
+  {
+    "revision": "524998766a0cd0d8eb75b58351fad5f8",
+    "url": "/static/media/slide-1.52499876.jpg"
+  },
+  {
+    "revision": "d4164d99062c09a5061634a86d7bdc0a",
+    "url": "/static/media/slide-2.d4164d99.jpg"
+  },
+  {
+    "revision": "1453a23102a895afef1d46fad0c10f4c",
+    "url": "/static/media/slide-3.1453a231.jpg"
+  },
+  {
+    "revision": "d365040d7a9b9882b798dfd6701b2c2e",
+    "url": "/static/media/templates.d365040d.png"
+  },
+  {
+    "revision": "cd8524e9fb1c0f5cce834a10930531c8",
+    "url": "/static/media/thiago-farias.cd8524e9.jpg"
+  },
+  {
+    "revision": "467c1674ba22beac8d518444d82bc2e4",
+    "url": "/static/media/topo_3_clicks.467c1674.png"
+  },
+  {
+    "revision": "b51fc5eee3e1aea36eb5d29eed766601",
+    "url": "/static/media/wilck-gomes.b51fc5ee.jpg"
+  }
+]);
